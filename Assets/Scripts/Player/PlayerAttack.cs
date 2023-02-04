201efc475c93b02998bdf4539696d71bf4ba13c6
alexandrechevalier1 <72023610+alexandrechevalier1@users.noreply.github.com>
2023-02-04 16:39:42 +0100

    feat(GAMEPLAY):
    player quick and heavy attack, remove lot's of bug and weird comportement add ressources

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     private float CoolDown;
-    public float StartCoolDown;
+    private float CoolDownValue;
+    public float StartCoolDownQuick;
+    public float StartCoolDownHeavy;
 
     public Transform attackPos;
     public LayerMask whatIsEnemies;
     public float attackRange;
-    public int damage;
-    public Animator anim;
+    public float QuickAttackDamage;
+    public float HeavyAttackDamage;
+    private float damage;
+
+    private bool Attack = false;
+
+    public Animator attack;
     public AudioSource swordNoise;
 
     // Start is called before the first frame update
@@ -22,17 +29,36 @@ public class PlayerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(CoolDown <= 0){
-            if(Input.GetKey(KeyCode.Mouse0)){
-                Collider2D[] SwordHitBox = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
-                anim.SetBool("isAttack", true);
-                swordNoise.Play();
-                for (int i = 0; i < SwordHitBox.Length; i++) {
-                    SwordHitBox[i].GetComponent<Enemy>().TakeDamage(damage);
-                }
-                StartCoroutine(AtkTime());
+        Collider2D[] SwordHitBox = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+        float waitTimeBefore = 0.0f;
+        float waitTimeAfter = 0.0f;
+        if (CoolDown <= 0){
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                Attack = true;
+                damage = QuickAttackDamage;
+                attack.SetBool("isAttack", true);
+                attack.SetBool("isHeavy", false);
+                waitTimeBefore = 0.05f;
+                waitTimeAfter = 0.2f;
+                CoolDownValue = StartCoolDownQuick;
+
             }
-            CoolDown = StartCoolDown;
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                Attack = true;
+                damage = HeavyAttackDamage;
+                attack.SetBool("isAttack", true);
+                attack.SetBool("isHeavy", true);
+                waitTimeBefore = 0.35f;
+                waitTimeAfter = 0.2f;
+                CoolDownValue = StartCoolDownHeavy; ;
+            }
+            if (Attack) {
+                swordNoise.Play();
+                StartCoroutine(AtkTime(SwordHitBox, waitTimeBefore, waitTimeAfter));
+                CoolDown = CoolDownValue;
+            }
         } 
         else
         {
@@ -40,10 +66,17 @@ public class PlayerAttack : MonoBehaviour
         }
     }
 
-    IEnumerator AtkTime()
+    IEnumerator AtkTime(Collider2D[] SwordHitBox, float wiatTimeBefore, float WaitTimeAfter)
     {
-        yield return new WaitForSeconds(0.5f);
-        anim.SetBool("isAttack", false);
+        yield return new WaitForSeconds(wiatTimeBefore);
+        for (int i = 0; i < SwordHitBox.Length; i++)
+        {
+            SwordHitBox[i].GetComponent<Enemy>().TakeDamage(damage);
+        }
+        yield return new WaitForSeconds(WaitTimeAfter);
+        attack.SetBool("isAttack", false);
+        attack.SetBool("isHeavy", false);
+        Attack = false;
     }
     private void OnDrawGizmosSelected()
     {
--- Assets/Scripts/WomenProprity.cs
+++ Assets/Scripts/WomenProprity.cs
@@ -15,14 +15,19 @@ public class WomenProprity : MonoBehaviour
         Debug.Log("ccououc");
         if(type == "orc")
         {
-            attack.damage *= 2;
+            attack.QuickAttackDamage *= 1.2f;
+            attack.HeavyAttackDamage *= 1.6f;
         }
         if(type == "elf")
         {
+            attack.QuickAttackDamage *= 1.6f;
+            attack.HeavyAttackDamage *= 0.80f;
             movement.moveSpeed *= 1.50f;
         }
         if(type == "barbarian")
         {
+            attack.QuickAttackDamage *= 1.2f;
+            attack.HeavyAttackDamage *= 1.2f;
             health.myHealth *= 1.75f;
             health.currentHealth = health.myHealth;
         }
